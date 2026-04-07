e2f4e1461a30a098fd3cd0c66621744c62c13d23
oussama-dalhi <oussamadalhi00@gmail.com>
2026-04-07 16:07:59 +0100

    initial commit - obdII project

---
+++ src/script.js
@@ -0,0 +1,44 @@
+const endpoint = './src/codes.json';
+const searchInput = document.querySelector('#dtc');
+const searchButton = document.querySelector('#search-button');
+const resultDisplay = document.querySelector('.result-field');
+const allCodes = [];
+fetch(endpoint)
+.then(codes => codes.json())
+.then(data => allCodes.push(...data));
+function findMatches(codeToMatch) {
+    if(!codeToMatch) return [];
+    const regex = new RegExp(codeToMatch, 'gi');
+    const matches = allCodes.filter(dtc => {
+        return dtc['Code'].match(regex);
+    });
+       const unique = Array.from(
+        new Map(matches.map(d => [d.Code, d])).values()
+    );
+    return unique;
+}
+
+function displayMatchedCode() {
+    const result = findMatches(searchInput.value.toUpperCase().trim());
+    if (result.length === 0) {
+    resultDisplay.innerHTML = `<p class = 'notfound'>No results found</p>`;
+    return;
+}
+    const displayHtml = result.map(dtc => {
+        return `
+           <h3 class = "code">Fault Code ${dtc['Code']} : <br></h3>
+           <p class = "description">${dtc['Description']}<a href="https://www.google.com/search?q=Fault+Code+${dtc['Code']}" target="_blank">
+  Learn more
+</a></p>
+        `
+    }).join('');
+    resultDisplay.innerHTML = `<p>Scanning...</p>`;
+    setTimeout(() => {
+        resultDisplay.innerHTML = displayHtml;
+    }, 500);
+    
+}
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    displayMatchedCode();
+});
